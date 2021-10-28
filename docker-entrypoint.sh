5492244e7f3cfdfac792624ccfef0c7c1bcfed3b
Maxim Oransky <maxim.oransky@gmail.com>
2021-10-29 00:57:16 +0300

    debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$@" && ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
+if [[ ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
     set -x
+    echo "init test db"
 
     cockroach start-single-node --insecure
     cockroach_pid=$!
@@ -10,6 +11,8 @@ if [[ -z "$@" && ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
     cockroach sql --insecure -e "CREATE USER IF NOT EXISTS ${DB_USER}; CREATE DATABASE IF NOT EXISTS ${DB_NAME}; GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USER}"
 
     kill ${cockroach_pid}
+else
+    echo "don't init test db"
 fi
 
 exec "$@"
