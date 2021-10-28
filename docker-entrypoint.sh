c4503673463b8eeebc7b95a180b963a493493949
Maxim Oransky <maxim.oransky@gmail.com>
2021-10-29 01:24:41 +0300

    pid maybe fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,13 @@ if [[ ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
     set -x
     echo "init test db"
 
-    pid_file=/tmp/cockroach.pid
-
-    cockroach start-single-node --insecure --background --pid-file=${pid_file}
+    cockroach start-single-node --insecure &
+    pid=$!
+    
     sleep 1 # FIXME:
 
     cockroach sql --insecure -e "CREATE USER IF NOT EXISTS ${DB_USER}; CREATE DATABASE IF NOT EXISTS ${DB_NAME}; GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USER}"
 
-    pid=$( cat ${pid_file} )
     kill ${pid}
     wait ${pid}
 else
