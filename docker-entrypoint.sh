4254caf6752442b0d6c5b51a5c7d31c6f66a8619
Maxim Oransky <maxim.oransky@gmail.com>
2021-10-29 00:38:44 +0300

    Check image step

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+if [[ -z "$@" && ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
+    set -x
+
+    cockroach start-single-node --insecure
+    cockroach_pid=$!
+
+    cockroach sql --insecure -e "CREATE USER IF NOT EXISTS ${DB_USER}; CREATE DATABASE IF NOT EXISTS ${DB_NAME}; GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USER}"
+
+    kill ${cockroach_pid}
+fi
+
+DB_NAME: chatnode
+DB_USER: chatnode
+
 exec "$@"
