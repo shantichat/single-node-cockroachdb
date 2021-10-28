23df05d9a4f2abed63827481c6c6d71ce7dd4987
Maxim Oransky <maxim.oransky@gmail.com>
2021-10-29 00:59:30 +0300

    init fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [[ ! -z "${DB_USER}" && ! -z "${DB_NAME}" ]]; then
     set -x
     echo "init test db"
 
-    cockroach start-single-node --insecure
+    cockroach start-single-node --insecure &
     cockroach_pid=$!
 
     cockroach sql --insecure -e "CREATE USER IF NOT EXISTS ${DB_USER}; CREATE DATABASE IF NOT EXISTS ${DB_NAME}; GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USER}"
